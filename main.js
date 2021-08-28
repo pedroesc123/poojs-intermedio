44841670957b91eec6761892cec57647f3d8b499
Pedro Espinoza Cordero <pedro.espinoza.c@uni.pe>
2021-08-28 11:53:26 -0500

    Trabajando con propiedades estaticas

--- main.js
+++ main.js
@@ -9,6 +9,49 @@ const juan = {
     }
 };
 
-console.log(Object.keys(juan));
-console.log(Object.getOwnPropertyNames(juan));
-console.log(Object.entries(juan));
+// console.log(Object.keys(juan));
+// console.log(Object.getOwnPropertyNames(juan));
+// console.log(Object.entries(juan));
+
+// Object.defineProperty(juan, "", {
+//     value: "",
+//     enumerable:true,
+//     writable: true,
+//     configurable:true,
+// })
+
+
+// Object.defineProperty(juan, "pruebaNasa", {
+//     value: "Extraterrestres",
+//     enumerable:false,
+//     writable: false,
+//     configurable:false,
+// })
+
+
+// Object.defineProperty(juan, "navigator", {
+//     value: "Chrome",
+//     enumerable:false,
+//     writable: true,
+//     configurable:true,
+// })
+
+
+// Object.defineProperty(juan, "editor", {
+//     value: "VSCode",
+//     enumerable:true,
+//     writable: false,
+//     configurable:true,
+// })
+
+
+// Object.defineProperty(juan, "terminal", {
+//     value: "WSL",
+//     enumerable:true,
+//     writable: true,
+//     configurable:false,
+// })
+
+Object.seal(juan);
+
+console.log(Object.getOwnPropertyDescriptors(juan));
